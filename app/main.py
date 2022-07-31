986c51aff557ba12146a68de518ba8ba894828d7
Alexander Samusev <alvic@Alexs-MacBook-Pro.local>
2022-07-31 20:03:07 +0200

    added some stuff, read pr

--- app/main.py
+++ app/main.py
@@ -12,10 +12,16 @@ import datetime
 from time import sleep
 
 logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 TOKEN = os.getenv("BOT_TOKEN")
+
+START_MESSAGE = """
+Hi! Just send me a youtube link and I'll send you an audio from it.
+⚠️ Pay attention: if I send several files start playing them from the last one ⚠️
+The reason: telegram plays audio files from bottom upwards.
+"""
 
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
@@ -85,19 +91,40 @@ def cleanup(id):
 
 
 def is_youtube_url(text):
-    if "https://www.youtube.com" in text or "https://m.youtube.com" in text:
-        return True
-    return False
+    """
+    Checks that text contains a youtube link
+    :param text: any text
+    """
+    youtube_links = [
+        "https://www.youtube.com",
+        "https://m.youtube.com",
+        "https://youtu.be",
+    ]
+    return any(youtube_link in text for youtube_link in youtube_links)
+
+
+def trim_link(link):
+    """
+    Trims link to get direct link to the video
+    Reason: If the link contains list id of playlist youtube-dl will download parts of it
+    :param link: youtube link
+    """
+    return link.split("&")[0]
 
 
 def calculate_file_size(filepath: str) -> int:
     """
     Returns filesize in MB
+    :param filepath: path to file
     """
     return os.stat(filepath).st_size / (1024 * 1024)
 
 
 def get_audio_duration(audiofile):
+    """
+    Returns duration of audiofile
+    :param audiofile: path to audiofile
+    """
     cmd = f"ffprobe -v quiet -print_format json -show_format -show_streams -print_format json {audiofile} | tr -d '\n'"
     try:
         full_audio_info_raw = subprocess.run(
@@ -111,7 +138,13 @@ def get_audio_duration(audiofile):
     return int(float(duration))
 
 
-def divide_file_into_parts(number_of_parts, duration, id):
+def divide_audio_into_parts(number_of_parts, duration, id):
+    """
+    Divides audio to several parts depending on duration
+    :param number_of_parts: number of parts the file should be divided
+    :param duration: duration of audio
+    :param id: id of audio file
+    """
     duration_of_one_part = duration // number_of_parts
     for i in range(0, number_of_parts):
         t1 = str(datetime.timedelta(seconds=i * int(duration_of_one_part)))
@@ -126,6 +159,7 @@ def divide_file_into_parts(number_of_parts, duration, id):
 
 def main():
     preflight()
+    logging.info(f"YTAP-Bot has started")
     bot = telegram.Bot(TOKEN)
     message_id = 0
     while True:
@@ -136,15 +170,14 @@ def main():
             message = updates[i]["message"]["text"]
             print(f"User id: {user_id}, message: {message}, message id: {message_id}")
             if message in "/start":
-                bot.send_message(
-                    user_id,
-                    "Hi! Just send me a youtube link and I'll send you an audio from it",
-                )
+                bot.send_message(user_id, START_MESSAGE)
             if is_youtube_url(message):
                 logging.info(f"Got a youtube link {message}")
                 bot.send_message(user_id, "Preparing video")
+                link = trim_link(message)
+                logging.info(f"Trimmed link to {link}")
                 id = id_generator()
-                video_name = download_video(id, message)
+                video_name = download_video(id, link)
                 audiofile = f"/tmp/audio-{id}.mp3"
                 get_audio_from_video(id)
                 # Telegram doesn't support media files > 50 MB for bots to send
@@ -158,8 +191,13 @@ def main():
                 else:
                     number_of_parts = int(filesize // 45 + 1)
                     audio_duration = get_audio_duration(audiofile)
-                    divide_file_into_parts(number_of_parts, audio_duration, id)
-                    for i in range(0, number_of_parts):
+                    divide_audio_into_parts(number_of_parts, audio_duration, id)
+                    bot.send_message(
+                        user_id,
+                        f"Sending several files. Start playing them from the last one",
+                    )
+                    # Sending file in reverse order, because telegram plays audio from the bottom up
+                    for i in range((number_of_parts - 1), -1, -1):
                         bot.send_audio(
                             user_id,
                             audio=open(f"/tmp/audio-{id}_part{i}.mp3", "rb"),
@@ -167,6 +205,10 @@ def main():
                         )
                 cleanup(id)
             else:
+                bot.send_message(
+                    user_id,
+                    f"Couldn't find a youtube link in your message: {message}",
+                )
                 logging.info(f"Got a message: {message}")
         if message_id:
             updates = bot.get_updates(offset=message_id + 1)
